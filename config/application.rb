fd87b6463362e7fb3c1898fcddc090c2798ee9a2
Shakeel Rauf <shkeelrauf@gmail.com>
2018-12-24 12:28:56 +0500

    Add database configuration.

--- config/application.rb
+++ config/application.rb
@@ -2,21 +2,17 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require 'dotenv' ; Dotenv.load ".env.local", ".env.#{Rails.env}", ".env"
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require 'dotenv'
-Dotenv.load
-
-
-Dotenv::Railtie.load
 module Cashfortrash
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
-Dotenv::Railtie.load
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
