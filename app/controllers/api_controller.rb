20a19db9ae533193d806c13e52d03e9a002ce553
Shakeel Rauf <shkeelrauf@gmail.com>
2018-12-26 11:25:42 +0500

    Msgs json

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,11 @@
 class ApiController < ApplicationController
+	include Api::V1::MsgConst
+
+  def render_json_response(resource, status)
+    render json: resource.to_json, status: status, adapter: :json_api
+  end
+
+  #usage
+  # return render_json_response({:error => "missing_params", :success => false}, :ok) if params[:content].nil?
+	
 end
