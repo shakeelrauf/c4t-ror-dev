69f3d8248586d0fd64df08903c32e7034ccb5464
Shakeel Rauf <shkeelrauf@gmail.com>
2019-01-01 18:58:23 +0500

    Fixing find_by_id method.

--- app/models/concerns/methods.rb
+++ app/models/concerns/methods.rb
@@ -2,7 +2,20 @@ module Methods
 	extend ActiveSupport::Concern
 	included do
 		def self.find_by_id id
-			self.send("find_by_id#{name}", id)
+			if ["Customer","Business"].include?(name)
+				id_key = "Client"
+			elsif name == "Heardofus"
+				id_key = "HeardOfUs"
+			elsif name == "QuoteCar"
+				id_key = "QuoteCars"
+			elsif name == "Setting"
+				id_key = "Settings"
+			elsif name == "VehicleInfo"
+				id_key = "VehiculeInfo"
+			else
+				id_key = name
+			end
+			self.send("find_by_id#{id_key}", id)
 		end
 
     def self.run_sql_query(query, offset=nil , limit=nil)
