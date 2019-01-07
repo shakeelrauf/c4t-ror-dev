aa31b98cf4767844b7b34fab3f3b78ec69fe8707
Hamza Satti <hamza.satti@phaedrasolutions.com>
2019-01-07 12:04:47 +0500

    Changes for status change

--- app/controllers/quote_controller.rb
+++ app/controllers/quote_controller.rb
@@ -18,4 +18,28 @@ class QuoteController < ApplicationController
 		@heardsofus = Heardofus.all
 		@cars = QuoteCar.all
   end
+
+
+  def update_quote_status
+    if (params[:status])
+      quotes = Quote.includes(:customer).where(idQuote: params[:no])
+      if quotes.present?
+        results = quotes.first.update(
+          idStatus: params[:status],
+          dtStatusUpdated: Time.now
+        )
+      end
+      r_quote = Quote.includes(:customer).where(idQuote: params[:no])
+      # If status is «in Yard», send sms to customer for know his appreciation.
+      if (params[:status] == 6)
+        # Check if sms already sent.
+        if (!r_quote.first.isSatisfactionSMSQuoteSent && r_quote.first.customer.cellPhone)
+          sms = TwilioTextMessenger.new "Hello. This is CashForTrash. We recently bought your car. We want to know your satisfaction. On a scale of 1 to 10, how much did you appreciate our service? Please respond with a number.", "4388241370"
+          sms.call
+          quotes.update(isSatisfactionSMSQuoteSent: 1)
+        end
+      end
+    end
+  end
+
 end
