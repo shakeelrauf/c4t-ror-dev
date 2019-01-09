996e8772af59632999350c49371f5f31bc8ef0c5
Talha Waseem <talha.waseem@phaedrasolutions.com>
2019-01-09 18:50:26 +0400

    settings label settings

--- app/helpers/setting_helper.rb
+++ app/helpers/setting_helper.rb
@@ -1,2 +1,21 @@
 module SettingHelper
+
+	def label(name)
+	  label = ""
+	  if name == "steelPrice"
+	        label = "Price Of Steel"
+		elsif name == "freeDistance"
+		  label = "Free Distance In Km"
+		elsif name == "excessPrice"
+		  label = "Price $ Per Additional Km"
+		elsif name == "catalysorPrice"
+		  label = "Value Of A Catalytic Converter"
+		elsif name == "wheelPrice"
+		  label = "Value Of A Wheel"
+		elsif name == "batteryPrice"
+		  label = "Value Of A Battery"
+		end
+    label
+	end
+
 end
