8c2baa3e95b02b7b8a258c12de6570e5a0c29e23
Hamza Satti <hamza.satti@phaedrasolutions.com>
2019-01-09 18:10:53 +0500

    Changes for cars

--- app/controllers/quotecars_controller.rb
+++ app/controllers/quotecars_controller.rb
@@ -10,14 +10,26 @@ class QuotecarsController < ApplicationController
  	end
 
  	def create_car
- 		@car = VehicleInfo.create(year: params[:years], make: params[:make], model: params[:model], trim: params[:trim], body: params[:body], drive: params[:drive], transmission: params[:transmission], seats: params[:seats],doors: params[:doors], weight: params[:weight])
- 		if @car.save
+    @car = ApiCall.post("/vehicles", form_body(params), headers )
  			redirect_to "/cars"
- 		end
  	end
 
  	def list_cars
  		@cars = ApiCall.get("/cars",{}, headers)
- 		# @cars = QuoteCar.includes([:information, :address, :quote => [:customer, :dispatcher, :status]]).all
  	end
+
+	def form_body(params)
+    {
+      "year":      					params[:year],
+      "make": 		 					params[:make],
+      "model": 		 					params[:model],
+      "trim": 	 	 					params[:trim],
+      "body": 		 					params[:body],
+      "drive": 		 					params[:drive],
+      "transmission": 	 	  params[:transmission],
+      "seats": 		 					params[:seats],
+      "doors": 	 	  				params[:doors],
+      "weight": 		 				params[:weight]
+    }
+  end
 end
