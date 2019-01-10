0a717a8c19399426df7485566810d01a35784235
Talha Waseem <talha.waseem@phaedrasolutions.com>
2019-01-10 10:34:27 +0400

    quote query and datatable search

--- app/controllers/api/v1/quote_controller.rb
+++ app/controllers/api/v1/quote_controller.rb
@@ -272,7 +272,9 @@ class Api::V1::QuoteController < ApiController
     if (params[:filter].class.to_s != "NilClass")
       filter = "%" + params[:filter] + "%"
     end
-    lstQuotes = Quote.includes(:dispatcher, :customer, :status).where(idClient: params[:no]).order('dtCreated DESC').offset(offset).limit(30).to_json(include: [:dispatcher, :customer, :status])
+    lstQuotes = Quote.includes(:dispatcher, :customer, :status).order('dtCreated DESC').offset(offset).limit(30).to_json(include: [:dispatcher, :customer, :status])
+    # it should be this query which is written down
+    # lstQuotes = Quote.includes(:dispatcher, :customer, :status).where(idClient: params[:no]).order('dtCreated DESC').offset(offset).limit(30).to_json(include: [:dispatcher, :customer, :status])
     # lstQuotes = Quote.includes(:dispatcher, :customer, :status).where("(note LIKE ?  OR status.name LIKE ? OR dispatcher.firstName LIKE ? OR dispatcher.lastName LIKE ? OR reference  LIKE ? ) AND idClient = ?", filter, filter, filter, filter, filter, params[:no]).order('DESC').offset(offset).limit(30)
 
     # lstQuotes.each do |quote|
