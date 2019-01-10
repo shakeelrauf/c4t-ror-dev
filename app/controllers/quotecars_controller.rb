69eb9d4781d2d11ae97b7ab42daf6d24e86be206
Hamza Satti <hamza.satti@phaedrasolutions.com>
2019-01-10 16:19:22 +0500

    Changes for bug fix

--- app/controllers/quotecars_controller.rb
+++ app/controllers/quotecars_controller.rb
@@ -43,8 +43,12 @@ class QuotecarsController < ApplicationController
 	    end
  			returned = {}
 	    returned[:results] = groups
-	    returned[:limit] = 15
-	    returned[:offset] = 0
+	    returned[:pagination] = {}
+	    if(groups.length != 30)
+	      returned[:pagination][:more] = false
+	    else
+	      returned[:pagination][:more] = true
+	    end
 	    respond_json(returned)
 	  end
  	end
