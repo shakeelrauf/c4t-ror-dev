36308d5eb95b95024f072f9f7fb6357595991b6b
Shakeel Rauf <shkeelrauf@gmail.com>
2019-01-22 11:52:52 +0500

    Refactory settings.

---
+++ app/controllers/concerns/settings.rb
@@ -0,0 +1,33 @@
+module Settings
+  def update_settings
+    @settings = Setting.all
+    @settings.each do |setting|
+      if setting.name == "steelPrice"
+        setting.value = params[:steelPrice]
+      elsif setting.name == "freeDistance"
+        setting.value = params[:freeDistance]
+      elsif setting.name == "excessPrice"
+        setting.value = params[:excessPrice]
+      elsif setting.name == "catalysorPrice"
+        setting.value = params[:catalysorPrice]
+      elsif setting.name == "wheelPrice"
+        setting.value = params[:wheelPrice]
+      elsif setting.name == "batteryPrice"
+        setting.value = params[:batteryPrice]
+      elsif setting.label == "bonus"
+        setting.value = params["--Bronze--bonus"]
+      elsif setting.label == "bonus-type"
+        setting.value = params["--Bronze--bonus-type"]
+      elsif setting.label == "bonus-1"
+        setting.value = params["--Silver--bonus"]
+      elsif setting.label == "bonus-type-1"
+        setting.value = params["--Silver--bonus-type"]
+      elsif setting.label == "bonus-2"
+        setting.value = params["--Gold--bonus"]
+      elsif setting.label == "bonus-type-2"
+        setting.value = params["--Gold--bonus-type"]
+      end
+      setting.save
+    end
+  end
+end
