80d55c7e881569cbcc9ab26be7c88d3e2072e7db
Patrice Gagnon <patrice@tenaya.ca>
2019-01-23 11:08:00 -0500

    Distance work.

--- app/controllers/distance_controller.rb
+++ app/controllers/distance_controller.rb
@@ -3,9 +3,25 @@ class DistanceController < ApplicationController
 
 	def distance
     postal = params[:postal].gsub(/\s/,'')
-    dist = ApiCall.get("/distance/#{postal}", {}, headers)
-    puts dist
-    respond_json(dist)
+    # This api will be used by the mobile
+    dist_res = ApiCall.get("/distance/#{postal}", {}, headers)
+    dist = JSON.parse(dist_res)
+    puts dist_res
+    if (dist.present? &&
+        dist['rows'].present? &&
+        dist['rows'].length > 0 &&
+        dist['rows'][0]['elements'].present? &&
+        dist['rows'][0]['elements'].length > 0 &&
+        dist['rows'][0]['elements'][0].present? &&
+        dist['rows'][0]['elements'][0]['distance'].present? &&
+        dist['rows'][0]['elements'][0]['distance']['value'].present?)
+
+      # Find the distance value
+      d = (dist['rows'][0]['elements'][0]['distance']['value'].to_i / 1000).to_i
+      respond_json(d)
+    else
+      respond_json("-1")
+    end
   end
 
 end
