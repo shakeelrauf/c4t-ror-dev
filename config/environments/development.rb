9b9a5b56966d7d09efce8a321a82710d02c7018d
Talha Waseem <talha.waseem@phaedrasolutions.com>
2019-01-23 10:23:08 +0500

    mailer cleaning

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'talha.waseem@phaedrasolutions.com',
-    password:             'talha123',
+    user_name:            'user@example.com',
+    password:             'password',
     authentication:       :plain,
     enable_starttls_auto: true
   }
