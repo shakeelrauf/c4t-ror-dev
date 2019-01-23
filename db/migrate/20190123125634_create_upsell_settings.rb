982784293af62b8a6ec45f889d8c015cb1e42985
Hamza Satti <hamza.satti@phaedrasolutions.com>
2019-01-23 18:28:03 +0500

    Changes for new table added

--- db/migrate/20190123125634_create_upsell_settings.rb
+++ db/migrate/20190123125634_create_upsell_settings.rb
@@ -3,8 +3,7 @@ class CreateUpsellSettings < ActiveRecord::Migration[5.2]
     create_table :upsell_settings do |t|
       t.integer :price_increase
       t.integer :deduction_money_figure
-      t.references :user, foreign_key: true
-
+      t.integer :user_id, foreign_key: true
       t.timestamps
     end
   end
