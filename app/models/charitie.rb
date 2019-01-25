4a78b9d2b91aa3efb67cbe38ae6d0525ebf01df5
Hamza Satti <hamza.satti@phaedrasolutions.com>
2019-01-25 13:08:57 +0500

    CHanges for charitie model refactored

--- app/models/charitie.rb
+++ app/models/charitie.rb
@@ -1,15 +1,5 @@
 class Charitie < ApplicationRecord
 	self.table_name = "Charities"
-	# validates :phone, presence: true, format: { with: /[0-9]*/ }, length: { minimum: 10 } #prev
-	# validates :email, presence: true, format: { with: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/ } #prev
 	validates :email, format: { with: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/ }, if: Proc.new { |charity| charity.email.present?}
-	# before_save :set_phone_format
-	
-	def set_phone_format
-    if self.phone.present? && self.phone.length == 10
-    	self.phone = self.phone[0..2] + " " + self.phone[3..5] + "-" + self.phone[6..-1]
-    elsif self.phone.present? && phone.length == 11
-    	self.phone =  self.phone[0] + "-" + self.phone[1..3] + " " + self.phone[4..6] + "-" + self.phone[7..-1]
-    end
-	end
+
 end
