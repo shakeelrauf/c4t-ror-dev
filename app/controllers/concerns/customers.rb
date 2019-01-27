8edf15c3d7c522c0c0ce27b25c1f289f3ce43910
Talha Waseem <talha.waseem@phaedrasolutions.com>
2019-01-27 14:34:25 +0500

    create code refactoring

--- app/controllers/concerns/customers.rb
+++ app/controllers/concerns/customers.rb
@@ -15,54 +15,18 @@ module Customers
   end
 
   def create_customer(params, current_user)
-    heardofus = Heardofus.find_or_create_by(type: params[:heardOfUs])
-      customDollarCar = 0
-      customDollarSteel = 0
-      customPercCar = 0
-      customPercSteel = 0
-      if current_user.present? && current_user.roles.downcase.eql?("admin")
-        customDollarCar = params[:customDollarCar] if params[:customDollarCar]
-        customDollarSteel = params[:customDollarSteel] if params[:customDollarSteel]
-        customPercCar = params[:customPercCar] if params[:customPercCar]
-        customPercSteel = params[:customPercSteel] if params[:customPercSteel]
-      end
-
       heardofus = Heardofus.find_or_create_by(type: params[:heardOfUs])
       if heardofus.present?
         client = Customer.find_or_initialize_by(phone: params[:phoneNumber])
         if client.new_record?
-          client.idHeardOfUs = heardofus.idHeardOfUs
-          client.firstName= params[:firstName]
-          client.lastName = params[:lastName]
-          client.email= params[:email]
-          client.type = params[:type]
-          client.extension = params[:extension]
-          client.cellPhone = params[:phoneNumber2]
-          client.secondaryPhone = params[:phoneNumber3]
-          client.note = params[:note]
-          client.grade = params[:grade]
-          client.customDollarCar = customDollarCar
-          client.customDollarSteel = customDollarSteel
-          client.customPercCar = customPercCar
-          client.customPercSteel = customPercSteel
-          client.save!
+          client = client_data(client, heardofus)
           params[:addresses].each do |a|
             if a.present?
-              a = a.as_json
-              prvc = a["province"].present? ? a["province"].upcase : a["province"]
-              newAddress = client.address.new( address: a["address"],city: a["city"],postal: a["postal"],province: prvc)
-              distance = JSON.parse(get_distance(newAddress))
-              newAddress.distance = (distance["rows"][0]["elements"][0]["distance"]["value"] + distance["rows"][0]["elements"][0]["distance"]["value"])
-              newAddress.save!
+              client_address(a, client)
             end
           end
           if !params[:type].eql?("Individual")
-            busi = client.build_business({
-              name: params[:name],
-              description: params[:description],
-              contactPosition: params[:contactPosition],
-              pstTaxNo: params[:pstTaxNo],
-              gstTaxNo: params[:gstTaxNo]})
+            busi = non_individual_business(client)
             if busi.save
               create_contacts(params, busi)
             end
@@ -72,6 +36,54 @@ module Customers
         end
       end
     client
+  end
+
+  def client_data(client, heardofus)
+    customDollarCar = 0
+    customDollarSteel = 0
+    customPercCar = 0
+    customPercSteel = 0
+    if current_user.present? && current_user.roles.downcase.eql?("admin")
+      customDollarCar = params[:customDollarCar] if params[:customDollarCar]
+      customDollarSteel = params[:customDollarSteel] if params[:customDollarSteel]
+      customPercCar = params[:customPercCar] if params[:customPercCar]
+      customPercSteel = params[:customPercSteel] if params[:customPercSteel]
+    end
+    client.idHeardOfUs = heardofus.idHeardOfUs
+    client.firstName= params[:firstName]
+    client.lastName = params[:lastName]
+    client.email= params[:email]
+    client.type = params[:type]
+    client.extension = params[:extension]
+    client.cellPhone = params[:phoneNumber2]
+    client.secondaryPhone = params[:phoneNumber3]
+    client.note = params[:note]
+    client.grade = params[:grade]
+    client.customDollarCar = customDollarCar
+    client.customDollarSteel = customDollarSteel
+    client.customPercCar = customPercCar
+    client.customPercSteel = customPercSteel
+    client.save!
+    client
+  end
+
+  def client_address(a, client)
+    a = a.as_json
+    prvc = a["province"].present? ? a["province"].upcase : a["province"]
+    newAddress = client.address.new( address: a["address"],city: a["city"],postal: a["postal"],province: prvc)
+    distance = JSON.parse(get_distance(newAddress))
+    newAddress.distance = (distance["rows"][0]["elements"][0]["distance"]["value"] + distance["rows"][0]["elements"][0]["distance"]["value"])
+    newAddress.save!
+  end
+
+  def non_individual_business(client)
+    busi = client.build_business({
+      name: params[:name],
+      description: params[:description],
+      contactPosition: params[:contactPosition],
+      pstTaxNo: params[:pstTaxNo],
+      gstTaxNo: params[:gstTaxNo]})
+    busi
   end
 
   def update_customer(params, current_user, id)
