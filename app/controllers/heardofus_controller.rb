8e9f2e13fff90c73259f24c1eee9e694a6be9257
Hamza Satti <hamza.satti@phaedrasolutions.com>
2019-01-27 14:07:25 +0500

    Changes for settings and heardofus module refactoring indentation

--- app/controllers/heardofus_controller.rb
+++ app/controllers/heardofus_controller.rb
@@ -1,40 +1,40 @@
 class HeardofusController < ApplicationController
-	# before_action :authenticate_admin
-	before_action :login_required
-	before_action :check_heardofus, only: [:heardsofus, :update, :edit]
-	include Api::V1::MsgsConst
+  # before_action :authenticate_admin
+  before_action :login_required
+  before_action :check_heardofus, only: [:heardsofus, :update, :edit]
 
-	def create
-		hou = Heardofus.find_by_type(params[:type])
-		if hou.nil?
-			hou = Heardofus.new(type: params[:type])
-			hou.save!
-			flash[:success] = "Created"
-			redirect_to "/heardofus"
-		else
-			flash[:error] = "Name already exist"
-			redirect_to "/heardofus/add"
-		end
-	end
+  def create
+    hou = Heardofus.find_by_type(params[:type])
+    if hou.nil?
+      hou = Heardofus.new(type: params[:type])
+      hou.save!
+      flash[:success] = "Created"
+      redirect_to "/heardofus"
+    else
+      flash[:error] = "Name already exist"
+      redirect_to "/heardofus/add"
+    end
+  end
 
-	def get_heardsofus
- 		@heardsOfUs = Heardofus.all
-	end
+  def get_heardsofus
+    @heardsOfUs = Heardofus.all
+  end
 
-	def new
-	end
+  def new
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		@heardofus.update(type: params[:type])
-		flash[:success] = "Edited"
-		redirect_to "/heardofus"
-	end
+  def update
+    @heardofus.update(type: params[:type])
+    flash[:success] = "Edited"
+    redirect_to "/heardofus"
+  end
 
-	private
-	def check_heardofus
-		@heardofus = Heardofus.find_by_id(params[:no])
-	end
+  private
+
+  def check_heardofus
+    @heardofus = Heardofus.find_by_id(params[:no])
+  end
 end
