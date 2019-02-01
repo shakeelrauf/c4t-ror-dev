4a0eca78011c42477054d4241c25c7cfed0a30df
Hamza Satti <hamza.satti@phaedrasolutions.com>
2019-02-01 19:14:45 +0500

    Changes for bug fix

--- app/models/charitie.rb
+++ app/models/charitie.rb
@@ -1,6 +1,6 @@
 class Charitie < ApplicationRecord
 	self.table_name = "Charities"
-	validates :email, format: { with: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/ }, if: Proc.new { |charity| charity.email.present?}
+ 	validates :email, format: { with: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/ ,message: "Email is invalid." }, if: Proc.new { |charity| charity.email.present?}
 	before_save :validate_phone_dashes
 
 	def validate_phone_dashes
