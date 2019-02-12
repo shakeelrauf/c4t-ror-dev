ba365aad9a9a522ca38883fe4016fa81dc78cf27
Shakeel Rauf <shkeelrauf@gmail.com>
2019-02-12 16:03:22 +0500

    Fix

--- public/js/quote.js
+++ public/js/quote.js
@@ -856,7 +856,6 @@ function fillCustomer(data) {
     });
     $("#customer").data("id", data.idClient)
     $("#select2-phone-fi-container.select2-selection__rendered").text(data.phone.substr(0,3) + "-" + data.phone.substr(3,3) + "-" + data.phone.substr(6) + " " + data.firstName + " " + data.lastName);
-    $("select[name=phone] option:selected").text(data.phone + " " + data.firstName + " " + data.lastName);
     $("input[name=firstName]").val(data.firstName);
     $("input[name=lastName]").val(data.lastName);
     $("select[name=customerType]").val(data.type);
