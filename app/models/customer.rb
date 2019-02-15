ffb186da46dd4324143ed58fb88a4b809d7fe539
Shakeel Rauf <shkeelrauf@gmail.com>
2019-02-15 18:21:44 +0500

    Order the heard of us.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,7 +7,7 @@ class Customer < ApplicationRecord
 	has_many :satisfactions, class_name: 'Satisfication', foreign_key: "idClient"
 	has_many :quotes, class_name: 'Quote', foreign_key: 'idClient'
 
-	GET_HEARD_OF_COUNT = "SELECT heardsofus.type, COUNT(heardsofus.type ) AS count_heardsofus_type, `Clients`.`idHeardOfUs` AS clients_idheardofus FROM `Clients` INNER JOIN `HeardsOfUs` ON `HeardsOfUs`.`idHeardOfUs` = `Clients`.`idHeardOfUs` GROUP BY `Clients`.`idHeardOfUs`"
+	GET_HEARD_OF_COUNT = "SELECT heardsofus.type, COUNT(heardsofus.type ) AS count_heardsofus_type, `Clients`.`idHeardOfUs` AS clients_idheardofus FROM `Clients` INNER JOIN `HeardsOfUs` ON `HeardsOfUs`.`idHeardOfUs` = `Clients`.`idHeardOfUs` GROUP BY `Clients`.`idHeardOfUs` ORDER BY COUNT(heardsofus.type ) DESC"
 
 	def self.customers_heardofus
 		self.run_sql_query(GET_HEARD_OF_COUNT)
