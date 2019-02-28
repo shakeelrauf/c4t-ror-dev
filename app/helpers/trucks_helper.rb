1e2dfb62b0e4ad9a57c9c096ccdd72b88201d9dc
Talha Waseem <talha.waseem@phaedrasolutions.com>
2019-02-28 15:01:29 +0500

    truck index tweaks

--- app/helpers/trucks_helper.rb
+++ app/helpers/trucks_helper.rb
@@ -1,2 +1,19 @@
 module TrucksHelper
+
+  def flatbed_type(type)
+    val = "Aluminum"
+    if type == 0
+      val = "Steel"
+    end
+    val
+  end
+
+  def flatbed(flat)
+    val = "Yes"
+    if flat == false
+      val = "No"
+    end
+    val
+  end
+
 end
