ebe3c03a68ffb74e6f1b19f816f71ea1055a913c
Talha Waseem <talha.waseem@phaedrasolutions.com>
2019-03-05 16:14:56 +0500

    flash settings

--- public/js/all.js
+++ public/js/all.js
@@ -123,8 +123,12 @@ if ($(".the_form").length) {
         }
     })
 }
-$(document).on('turbolinks:load', function() {
-   $(window).on('popstate', function() {
-      location.reload(true);
-   });
+window.addEventListener( "pageshow", function ( event ) {
+  var historyTraversal = event.persisted ||
+                         ( typeof window.performance != "undefined" &&
+                              window.performance.navigation.type === 2 );
+  if ( historyTraversal ) {
+    // Handle page restore.
+    $(".growl-animated").hide();
+  }
 });
