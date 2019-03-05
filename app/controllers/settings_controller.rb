47ada930c952d77dc55473900bca93479a878330
Hamza Satti <hamza.satti@phaedrasolutions.com>
2019-03-05 18:29:14 +0500

    Changes for new condition added on setting module

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -14,8 +14,12 @@ class SettingsController < ApplicationController
   end
 
   def update
-    update_settings
-    flash[:success] = 'Setting is successfully Updated!'
+    if params[:max_purchaser_increase].to_i > params[:max_increase_with_admin_approval].to_i
+      flash[:danger] = "Max Increase With Admin Approval must be greater than Max purchaser Increase"
+    else
+      update_settings
+      flash[:success] = 'Setting is successfully Updated!'
+    end
     redirect_to settings_path
   end
 end
